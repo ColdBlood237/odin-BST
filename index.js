53aa53526b86513c66d1b4d3c8b4d9c10d6050c1
Pixel <petngalan@gmail.com>
2023-05-09 10:55:28 +0200

    Build the function that creates the binary search tree, and the function that prints it

--- index.js
+++ index.js
@@ -9,12 +9,41 @@ class Node {
 class Tree {
   constructor(array) {
     this.array = array;
-    this.root = buildTree();
+    this.root = null;
   }
 }
 
-function buildTree(dataArray) {
-  const sortedData = merge_sort(dataArray);
+function buildTree(dataArray, start, end) {
+  let sortedData = merge_sort(dataArray);
+  sortedData = [...new Set(sortedData)]; // remove duplicates
+
+  if (start > end) {
+    return null;
+  }
+
+  let mid = parseInt((start + end) / 2);
+  let node = new Node(sortedData[mid]);
+
+  node.left = buildTree(sortedData, start, mid - 1);
+  node.right = buildTree(sortedData, mid + 1, end);
+
+  return node;
+}
+
+let testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+prettyPrint(buildTree(testArray, 0, testArray.length - 1));
+
+function prettyPrint(node, prefix = "", isLeft = true) {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
 }
 
 function merge_sort(array) {
