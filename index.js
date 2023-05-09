6e0731b5880581ea5bee0a8d67c1a665f2aee63f
Pixel <petngalan@gmail.com>
2023-05-09 09:58:20 +0200

    Add the merge sort function to sort the array passed as argument to buildTree()

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 class Node {
-  constructor(data, left, right) {
+  constructor(data) {
     this.data = data;
-    this.left = left;
-    this.right = right;
+    this.left = null;
+    this.right = null;
   }
+}
+
+class Tree {
+  constructor(array) {
+    this.array = array;
+    this.root = buildTree();
+  }
+}
+
+function buildTree(dataArray) {
+  const sortedData = merge_sort(dataArray);
+}
+
+function merge_sort(array) {
+  if (array.length < 2) {
+    return array;
+  }
+
+  let left_half = array.slice(0, array.length / 2);
+  let right_half = array.slice(array.length / 2);
+  left_half = merge_sort(left_half);
+  right_half = merge_sort(right_half);
+
+  return merge(left_half, right_half);
+}
+
+// merge when left part and right part are both sorted already
+function merge(left_part, right_part) {
+  let merged_list = [];
+  let i = 0;
+  let j = 0;
+  while (i < left_part.length && j < right_part.length) {
+    if (left_part[i] < right_part[j]) {
+      merged_list.push(left_part[i]);
+      i++;
+    } else {
+      merged_list.push(right_part[j]);
+      j++;
+    }
+  }
+  for (; i < left_part.length; i++) {
+    merged_list.push(left_part[i]);
+  }
+  for (; j < right_part.length; j++) {
+    merged_list.push(right_part[j]);
+  }
+
+  return merged_list;
 }
